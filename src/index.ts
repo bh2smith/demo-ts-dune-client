9440f35b3c894aea8661fd82e480718f3de00400
Ben Smith <bh2smith@gmail.com>
2024-03-08 12:54:27 +0100

    updating repo

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import { QueryParameter, DuneClient } from "@cowprotocol/ts-dune-client";
+import { QueryParameter, DuneClient } from "@duneanalytics/client-sdk";
 
 const { DUNE_API_KEY } = process.env;
 
 const client = new DuneClient(DUNE_API_KEY ?? "");
 const queryID = 1215383;
-const parameters = [
+const params = [
   QueryParameter.text("TextField", "Plain Text"),
   QueryParameter.number("NumberField", 3.1415926535),
   QueryParameter.date("DateField", "2022-05-04 00:00:00"),
@@ -14,4 +14,4 @@ const parameters = [
 // Exclude the debug logs from lower level dependency.
 // console.debug = function () {};
 
-client.refresh(queryID, parameters).then((executionResult) => console.log(executionResult.result?.rows));
+client.runQuery(queryID, {query_parameters: params}).then((executionResult) => console.log(executionResult.result?.rows));
