98195531d9ed0495e9591b5b391a14d8ffa211f9
Ben Smith <bh2smith@gmail.com>
2024-03-29 09:52:30 +0100

    access changed files and alter query

--- queries/token_prices_1791490.sql
+++ queries/token_prices_1791490.sql
@@ -1,8 +1,11 @@
-select 
-    date_trunc('day', minute) as day, 
-    avg(price) as price
-from prices.usd 
-where blockchain = '{{Network}}' 
-and date_trunc('year', minute) = cast('{{YEAR}}-01-01' as timestamp)
-and contract_address = from_hex('{{TokenAddress}}')
-group by date_trunc('day', minute)
+select
+  date_trunc('day', minute) as day,
+  avg(price) as price
+from
+  prices.usd
+where
+  blockchain = 'ethereum'
+  and date_trunc('year', minute) = cast('{{YEAR}}-01-01' as timestamp)
+  and contract_address = from_hex('{{TokenAddress}}')
+group by
+  date_trunc('day', minute)
