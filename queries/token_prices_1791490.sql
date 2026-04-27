f3d351747f59f58b9125dba3bac1d45b5b038025
Benjamin Smith <bh2smith@users.noreply.github.com>
2026-04-27 14:37:52 +0200

    Fix typo in token_prices query (AN -> AND)

--- queries/token_prices_1791490.sql
+++ queries/token_prices_1791490.sql
@@ -6,6 +6,6 @@ FROM
 WHERE
   blockchain = '{{Network}}'
   AND date_trunc('year', minute) = CAST('{{YEAR}}-01-01' AS timestamp)
-  AN contract_address = from_hex('{{TokenAddress}}')
+  AND contract_address = from_hex('{{TokenAddress}}')
 GROUP BY
   date_trunc('day', minute)
