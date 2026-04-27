00d8ccfefed62938f1e0dd366e93342e0f7b07a2
Benjamin Smith <bh2smith@users.noreply.github.com>
2026-04-27 14:54:31 +0200

    Revert token_prices query to original

--- queries/token_prices_1791490.sql
+++ queries/token_prices_1791490.sql
@@ -6,6 +6,6 @@ FROM
 WHERE
   blockchain = '{{Network}}'
   AND date_trunc('year', minute) = CAST('{{YEAR}}-01-01' AS timestamp)
-  AND contract_address = from_hex('{{TokenAddress}}')
+  AN contract_address = from_hex('{{TokenAddress}}')
 GROUP BY
   date_trunc('day', minute)
